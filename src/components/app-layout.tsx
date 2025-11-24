0192028a4ec1013e6b9f4bec7bc10af6d405b3f6
فاقت لل <faqtll8@gmail.com>
2025-11-24 17:16:00 +0000

    انا اقصد ان الشاشة بيظاء يجب ان انزل الصفحة

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -70,7 +70,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
             </SidebarMenu>
           </SidebarContent>
         </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 lg:pr-14">
+        <div className="flex flex-col lg:pr-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <Sheet>
               <SheetTrigger asChild>
