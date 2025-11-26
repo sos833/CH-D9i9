bcb2f121dde4e15ac1a909ab5d639113551f90fe
فاقت لل <faqtll8@gmail.com>
2025-11-26 00:09:14 +0000

    Create a project Let's start with a name for your project dzhg-54e53 Pr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // NOTE: Replace this with your own Firebase configuration
 // This is a placeholder and will not work.
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000",
+  apiKey: 'AIzaSyChA08nI12YEU5sAU9n-vJbV4n7aU92Z0A',
+  authDomain: 'dzhg-54e53.firebaseapp.com',
+  projectId: 'dzhg-54e53',
+  storageBucket: 'dzhg-54e53.appspot.com',
+  messagingSenderId: '106955421596',
+  appId: '1:106955421596:web:1e5f037617b126c8585a9c'
 };
