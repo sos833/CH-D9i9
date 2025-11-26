f9e80963bcbe9ff6768c81ebd77db80df11015a1
فاقت لل <faqtll8@gmail.com>
2025-11-26 03:13:41 +0000

    الان اعطيني متغيرات المشروع البيئة

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,10 @@
 
-// NOTE: Replace this with your own Firebase configuration
-// This is a placeholder and will not work.
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAkyrM9WcCR6HodNIa-I_0C8yzy9jNtnEA',
-  authDomain: 'fleet-blend-477002-e1.firebaseapp.com',
-  projectId: 'fleet-blend-477002-e1',
-  storageBucket: 'fleet-blend-477002-e1.appspot.com',
-  messagingSenderId: '1034975329799',
-  appId: '1:1034975329799:web:b76d084037d2ad4eb674a2',
-  measurementId: 'G-X5CTDXN42M',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
